ecc7c6264fcb4b4f9a5891cda0fd804f03c13ce4
pkaela123 <pkaela123@gmail.com>
2023-12-26 17:57:50 -0500

    Added windspeed and humidity functionality

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ function updateWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
 
+  console.log(response.data);
   cityElement.innerHTML = response.data.name;
+
   descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
